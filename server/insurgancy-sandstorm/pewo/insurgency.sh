9bb80c2a02fe18954dc56121f044b8e99c0cf04e
pewo <peter.wirdemo@gmail.com>
2019-02-17 18:15:34 +0000

    chaned password location

--- server/insurgancy-sandstorm/pewo/insurgency.sh
+++ server/insurgancy-sandstorm/pewo/insurgency.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+PASS="`cat ~steam/gamepass.txt`"
 BIN=/home/steam/.steam/steamcmd/insurgency/Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping
 #ARGS="Oilfield?Scenario=Scenario_Refinary_Push_Security?MaxPlayers=20 -password=m -log=/tmp/steam.log -hostname=eri-nuke -QueryPort=27015"
 #ARGS="-password=m -log=/tmp/steam.log -hostname=eri-nuke -QueryPort=27015 -help"
@@ -7,5 +8,5 @@ BIN=/home/steam/.steam/steamcmd/insurgency/Insurgency/Binaries/Linux/InsurgencyS
 #ARGS="MaxPlayers=12&password=m -log -hostname=eri-nuke"
 #ARGS="-log -hostname=eri-nuke -QueryPort=27131"
 #ARGS="Oilfield?Scenario=Scenario_Refinery_Push_Security?password=m?MaxPlayers=10 -log -hostname=eri-pewo -QueryPort=27131"
-ARGS="Oilfield?Scenario=Scenario_Refinery_Push_Security?password=m?MaxPlayers=14 -log -hostname=eri-pewo -QueryPort=27131"
+ARGS="Oilfield?Scenario=Scenario_Refinery_Push_Security?password=${PASS}?MaxPlayers=14 -log -hostname=eri-pewo -QueryPort=27131"
 exec $BIN $ARGS
